059ae14a46aa42ec617571cd63e536e48b9853f7
EchoFUN <xukai.ken@gmail.com>
2013-08-08 17:09:00 +0800

    Finish the load module.

---
+++ src/core.js
@@ -0,0 +1,63 @@
+/**
+ *
+ *
+ *
+ */
+
+;(function(global) {
+  var doc = global.document;
+
+  function getEnv() {
+    var ua = navigator.userAgent, env = {};
+    return (env.ie = /MSIE|Trident/.test(ua)) || (env.unknown = true);
+  }
+
+  function createNode(name, attrs) {
+    var node = doc.createElement(name), attr;
+
+    for (attr in attrs) {
+      if (attrs.hasOwnProperty(attr)) {
+        node.setAttribute(attr, attrs[attr]);
+      }
+    }
+
+    return node;
+  }
+
+  function loadModule(url, async) {
+    var env = getEnv();
+
+    var node = createNode('script', {
+      src : url
+    });
+    node.async = async;
+
+    if (evn.ie) {
+      node.onreadystatechange = function() {
+        if (/loaded|complete/.test(node.readyState)) {
+          node.onreadystatechange = null;
+          loaded();
+        }
+      };
+    } else {
+      node.onload = loaded;
+    }
+  }
+  
+  function convertArray(o) {
+    return Array.prototype.slice.call(o); 
+  }
+
+  _ = customize = {
+    version : '0.0.1'
+  };
+  
+  _.loadModule = loadModule;
+  
+  var scripts = convertArray(document.scripts), bootstrap;
+  for(var script in scripts) {
+    console.log(script);
+  }
+
+})(this);
+
